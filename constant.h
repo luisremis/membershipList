282e877560e94f0d3e596a3d04b798928a8cd130
remis2 <remis2@illinois.edu>
2015-10-25 16:30:33 -0500

    Filed added

---
+++ constant.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#define BUFFER_MAX (521)
+#define NODES_NUMBER (7)
+#define K_FORWARD (4)
+
+//  in ms
+#define SLEEP_TIME (50)
+#define MAX_LATENCY (200*1000)	//0.1s
+
+#endif
